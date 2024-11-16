46d64faa1af4c571141fc652510282715767de28
Timofei <timabelov08@gmail.com>
2024-11-16 22:16:33 +0300

    some constants

---
+++ Simulation/simulationConstants.py
@@ -0,0 +1,10 @@
+import math
+
+# in Ф/m
+e_electicConstant = 8.8541878e-12
+# just coef
+k = 1 / (4 * math.pi * e_electicConstant)
+# in K (кулоны)
+electron_charge = -1 * 1.6021766e-19
+# in kg
+electron_mass = 9.109383e-31
