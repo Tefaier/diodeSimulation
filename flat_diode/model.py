some things changed - fall on cathode allowed, nets set allowed, max charge fixed, pref sum full calculation made, reverse effect of following charges made, rate of charge appearance added as constructor item

flat_diode/model.py
@@ -1,19 +1,29 @@
+import math
 from typing import List, Tuple
 
 import numpy as np
 
 ELECTRON_MASS = 9.109383719e-31
 ELECTRON_CHARGE = -1.602176634e-19
+ELECTRIC_CONSTANT = 8.85418781762039e-12 # E0
 COULOMB_CONSTANT = 8.9875517923e9  # 1/4piE0
-MAX_CATHODE_DT_CHARGE = 1  # think about value
+MAX_CHARGE_AT_CATHODE = -1e-10  # think about value
 
 
 class FlatDiode1DModel:
-    def __init__(self, voltage: float, cathode_radius: float, anode_radius: float, height: float, delta_time: float,
-                 amount_of_cells_between_cathode_and_anode: int):
-        zero_potential = 0  # todo
-        self.q_cathode = (-1 * (voltage + zero_potential) / (2 * COULOMB_CONSTANT) /
-                          np.log(anode_radius / cathode_radius))
+    zero_potential: float = 0  # todo
+    nets: List[Tuple[int, float]] = []
+
+    def __init__(
+            self,
+            voltage: float,  # positive value makes charge flow
+            cathode_radius: float,
+            anode_radius: float,
+            height: float,
+            delta_time: float,
+            amount_of_cells_between_cathode_and_anode: int,
+            charge_increase_rate: float # should be negative -> charge per second
+    ):
         self.dt = delta_time
         self.n = amount_of_cells_between_cathode_and_anode
         self.r1 = cathode_radius
@@ -22,22 +32,45 @@ class FlatDiode1DModel:
         self.big_charges = np.zeros(self.n)
         self.electric_field = np.zeros(self.n)
         self.velocities = np.zeros(self.n)
+        self.increase_rate = charge_increase_rate
+        self.set_voltage(voltage)
+
+    # nets with first value being radius and second value being its charge
+    def set_nets(self, nets: List[Tuple[float, float]]):
+        withSlots = [(self.round_to_cell(net[0]), net[1]) for net in nets]
+        withSlots = filter(lambda net: net[0] >= 0 and net[0] < self.n, withSlots)
+        self.nets = sorted(withSlots, key=lambda net: net[0])
+
+    def set_voltage(self, voltage: float):
+        self.q_cathode = (-1 * (voltage + self.zero_potential) / (2 * COULOMB_CONSTANT) /
+                          math.log(self.r2 / self.r1))
+
+    def calculate_pref_sum(self):
+        # pref sum is n+1 size with 0 showing cathode!
+        pref_sum = np.zeros(self.n + 1)
+        pref_sum[0] = self.q_cathode
+        net_pointer = 0
+        for i in range(1, self.n + 1):
+            pref_sum[i] = pref_sum[i-1] + self.big_charges[i-1]
+            if net_pointer < len(self.nets) and self.nets[net_pointer][0] == i - 1:
+                pref_sum[i] += self.nets[net_pointer][1]
+                net_pointer += 1
+        return pref_sum
 
     def iterate(self) -> float:
-        self.big_charges[0] += -1e-6 * self.dt
         electric_field = np.zeros(self.n)
         dr = (self.r2 - self.r1) / self.n  # delta r per cell
-        self.velocities[0] = start_velocity = dr / self.dt
-        if self.big_charges[0] > MAX_CATHODE_DT_CHARGE:
-            self.big_charges[0] = MAX_CATHODE_DT_CHARGE
-        pref_sum = 0
+
+        self.big_charges[0] += self.increase_rate * self.dt
+        if self.big_charges[0] < MAX_CHARGE_AT_CATHODE:  # because negative charge
+            self.big_charges[0] = MAX_CHARGE_AT_CATHODE
+        # self.velocities[0] = start_velocity = dr / self.dt
+        pref_sum = self.calculate_pref_sum()
         new_cords = {}
         for i in range(0, self.n):
-            if self.big_charges[i]:
+            if self.big_charges[i] != 0:
                 r_i = (self.r1 + i * dr)
-                electric_field[i] = (self.q_cathode * 2 * COULOMB_CONSTANT / r_i / self.h +
-                                     2 * COULOMB_CONSTANT / r_i / self.h * pref_sum)
-                pref_sum += self.big_charges[i]
+                electric_field[i] = (pref_sum[i] - pref_sum[-1] + pref_sum[i+1]) * (2 * COULOMB_CONSTANT) / (r_i * self.h)
                 self.velocities[i] += electric_field[i] * ELECTRON_CHARGE / ELECTRON_MASS * self.dt
                 new_i = self.round_to_cell(r_i + self.velocities[i] * self.dt)
                 if new_i in new_cords:
@@ -46,18 +79,22 @@ class FlatDiode1DModel:
                     new_cords[new_i] = [(self.big_charges[i], self.velocities[i])]
                 self.big_charges[i] = 0
                 self.velocities[i] = 0
+
         amperage = 0
         for new_i, arr in new_cords.items():
             new_q, new_v = self.compose(arr)
             if new_i >= self.n:
                 amperage += new_q
+            elif new_i < 0:
+                continue
             else:
                 self.big_charges[new_i] = new_q
                 self.velocities[new_i] = new_v
         return amperage / self.dt
 
     def round_to_cell(self, x):
-        assert (x > self.r1)
+        if x <= self.r1:
+            return -1  # fell on cathode
         if x >= self.r2:
             return self.n  # self.r2
         a = (x - self.r1) / (self.r2 - self.r1) * self.n
@@ -74,6 +111,6 @@ class FlatDiode1DModel:
         return new_q, sum_imp / new_q
 
 
-diode = FlatDiode1DModel(2e-2,1e-3, 3e-3, 100e-3, 1e-9, 10**4)
+diode = FlatDiode1DModel(2e-2,1e-3, 3e-3, 100e-3, 1e-9, 10**4, ELECTRON_CHARGE * 1e6)
 for i in range(200):
     print(diode.iterate())